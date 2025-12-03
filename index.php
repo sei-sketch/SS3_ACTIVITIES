a43cd241ddae28d4adeef1551af4c6f814a3d257
sei-sketch <seidrox1@gmail.com>
2025-12-04 02:47:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -23,6 +23,22 @@
     $paint = 12000;
     $misc = 20000;
 
+    $lumber10 = $lumber * 1.10;
+    $lumber15 = $lumber * 1.15;
+    $lumber20 = $lumber * 1.20;
+
 ?>
+
+<h2>Public Library Expansion Project</h2>
+<h3>Cost Estimates</h3>
+
+<table border="1">
+    <tr>
+        <th>Expenditures</th>
+        <th>Estimated Cost</th>
+        <th>10% Increase</th>
+        <th>15% Increase</th>
+        <th>20% Increase</th>
+    </tr>
 </body>
 </html>
